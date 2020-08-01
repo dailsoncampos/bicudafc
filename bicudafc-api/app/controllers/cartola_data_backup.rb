b65076cc92dd399620a98f87b6991d6dfa61c4dd
dailsoncampos <dcampos.dev@gmail.com>
2020-08-01 14:04:48 -0300

    definições de versionamento dos backups

--- bicudafc-api/app/controllers/cartola_data_backup.rb
+++ bicudafc-api/app/controllers/cartola_data_backup.rb
@@ -4,16 +4,41 @@ class CartolaDataBackup < ApplicationController
 
   def get_data(endpoint, *storage_path, file_name)
     response = self.class.get(endpoint, format: :plain)
-    backup_data(storage_path, file_name, response, endpoint)
+    backup_data(storage_path, file_name, response)
   end
 
   private
 
-  def backup_data(storage_path, file_name, content, endpoint)
+  def backup_data(storage_path, file_name, content)
     root = Backup::ROOT_STORAGE
-    storage_path.empty? ? file = File.new(root + file_name + '.json', 'w') : file = File.new(root + storage_path[0] + file_name + '.json', 'w')
-    file.puts(content)
-    file.close
-    puts 'Backup de realizado com sucesso!'
+    current_file = root + file_name + '.json'
+    
+    if !File.file?(current_file)
+      storage_path.empty? ? file = File.new(root + file_name + '.json', 'w') : file = File.new(root + storage_path[0] + file_name + '.json', 'w')
+      file.puts(content)
+      file.close
+      puts 'Backup de realizado com sucesso!'
+    else
+      storage_path.empty? ? new_file = File.new(root + file_name + '_new.json', 'w') : new_file = File.new(root + storage_path[0] + file_name + '_new.json', 'w')
+      new_file.puts(content)
+      new_file.close
+      puts 'Backup do novo arquivo realizado com sucesso!'
+      versioning_file(root, file_name)
+    end
+  end
+
+  def versioning_file(root, file_name)
+    current_path = root + file_name + '.json'
+    new_path = root + file_name + '_new.json'
+
+    current_file = File.read(current_path)
+    new_file = File.read(new_path)
+
+    delete_rename_file(current_path, new_path) unless current_file != new_file
+  end
+
+  def delete_rename_file(old_path, new_path)
+    File.delete(old_path)
+    File.rename(new_path, new_path=old_path)
   end
 end
