a37d85e48d0fab606d5282ebc81a8e084a19009b
dailsoncampos <dcampos.dev@gmail.com>
2020-08-04 14:22:12 -0300

    retirado método redundante

--- bicudafc-api/app/controllers/rounds_data_backup.rb
+++ bicudafc-api/app/controllers/rounds_data_backup.rb
@@ -1,33 +1,29 @@
 class RoundsDataBackup
   def initialize
     @cartola = CartolaDataBackup.new
-    @rounds_file = 'vendor/bkp/rounds.json'
-    File.file?(@rounds_file) ? rounds_data_updated?('/rodadas', 'rounds') : rounds_data('/rodadas', 'rounds')
+    @rounds_file = "#{Rails.root.join('vendor')}/bkp/rounds.json"
+    File.file?(@rounds_file) if rounds_data_updated?('/rodadas', 'rounds')
   end
 
   private
 
   def rounds_data_updated?(endpoint, file)
     @cartola.get_data(endpoint, file)
-  end
-
-  def rounds_data(endpoint, *storage_path, file_name)
-    @cartola.get_data(endpoint, *storage_path, file_name)
     see_current_round(@rounds_file)
   end
 
   def see_current_round(file)
-    current_date = Date.today.strftime("%Y-%m-%d")
+    current_date = Date.today
     rounds = File.read(file)
     rounds_parsed = JSON.parse rounds, symbolize_names: true
 
     rounds_parsed.each do |round|
       round_start_date = Date.parse(round[:inicio])
-      make_directory(round) if current_date >= round_start_date - 4.days
+      make_directory(round) if current_date >= round_start_date - 5.days
     end
   end
 
   def make_directory(round)
-    Dir.mkdir('vendor/bkp/round_' + round[:rodada_id].to_s) if !Dir.exist?('vendor/bkp/round_' + round[:rodada_id].to_s)
+    Dir.mkdir "#{Rails.root.join('vendor')}/bkp/round_#{round[:rodada_id].to_s}" unless File.directory?("#{Rails.root.join('vendor')}/bkp/round_#{round[:rodada_id].to_s}")
   end
 end
