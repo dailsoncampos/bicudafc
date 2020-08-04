34b2a2e91fa0cb5b370e80eaf040e4545c0df74f
dailsoncampos <dcampos.dev@gmail.com>
2020-08-04 12:10:00 -0300

    melhorada navegação em diretórios no projeto

--- bicudafc-api/app/controllers/cartola_data_backup.rb
+++ bicudafc-api/app/controllers/cartola_data_backup.rb
@@ -10,26 +10,25 @@ class CartolaDataBackup < ApplicationController
   private
 
   def backup_data(storage_path, file_name, content)
-    root = Backup::ROOT_STORAGE
-    current_file = root + file_name + '.json'
+    current_file = "#{Rails.root.join('vendor')}/bkp/#{file_name}.json"
     
     if !File.file?(current_file)
-      storage_path.empty? ? file = File.new(root + file_name + '.json', 'w') : file = File.new(root + storage_path[0] + file_name + '.json', 'w')
+      storage_path.empty? ? file = File.new("#{Rails.root.join('vendor')}/bkp/#{file_name}.json", "w") : file = File.new("#{Rails.root.join('vendor')}/bkp/#{storage_path[0]}#{file_name}.json", "w")
       file.puts(content)
       file.close
       puts 'Backup de realizado com sucesso!'
     else
-      storage_path.empty? ? new_file = File.new(root + file_name + '_new.json', 'w') : new_file = File.new(root + storage_path[0] + file_name + '_new.json', 'w')
+      storage_path.empty? ? new_file = File.new("#{Rails.root.join('vendor')}/bkp/#{file_name}_new.json", "w") : new_file = File.new("#{Rails.root.join('vendor')}/bkp/#{storage_path[0]}#{file_name}_new.json", "w")
       new_file.puts(content)
       new_file.close
       puts 'Backup do novo arquivo realizado com sucesso!'
-      versioning_file(root, file_name)
+      versioning_file(file_name)
     end
   end
 
-  def versioning_file(root, file_name)
-    current_path = root + file_name + '.json'
-    new_path = root + file_name + '_new.json'
+  def versioning_file(file_name)
+    current_path = "#{Rails.root.join('vendor')}/bkp/#{file_name}.json"
+    new_path = "#{Rails.root.join('vendor')}/bkp/#{file_name}_new.json"
 
     current_file = File.read(current_path)
     new_file = File.read(new_path)
