93c7f7189404ca5d5bd1802f6fe618b6144aa1f7
Dailson Campos <dcampos.dev@gmail.com>
2021-05-30 10:30:38 -0300

    build rounds file as main file to backup

--- bicudafc-api/app/controllers/cartola_data_backup.rb
+++ bicudafc-api/app/controllers/cartola_data_backup.rb
@@ -2,14 +2,10 @@ class CartolaDataBackup < ApplicationController
   include HTTParty
   base_uri "https://api.cartolafc.globo.com/"
 
-  def initialize(endpoint, file_name)
-    response = self.class.get(endpoint, format: :plain)
-    Folder.new({backup_path: Figaro.env.backup_path, content: response, file_name: file_name}).make
-    debugger
-
-    # file_path = "#{Figaro.env.backup_path}#{file_name}.json"
-
-    # write_file(response, file_path)
+  def get_rounds
+    content = self.class.get('/rodadas', format: :plain)
+    main_file = build_path('rounds')
+    write_file(content, main_file) unless File.file?(main_file)
   end
 
   def get_other_data(endpoint, file_name, *round_dir_path)
@@ -25,6 +21,11 @@ class CartolaDataBackup < ApplicationController
   end
 
   private
+
+  def build_path(file_name)
+    current_season = Folder.new({backup_path: Figaro.env.backup_path, content: content, file_name: file_name}).make
+    file_path = "#{current_season}/#{file_name}.json"
+  end
 
   def write_file(content, file_path)
     file = File.new(file_path, 'w')
